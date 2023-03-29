8d3b05a085af2694c8c38f389c7751dc9b1e3204
clauloro <114728388+clauloro@users.noreply.github.com>
2023-03-29 17:04:32 +0200

    prints

--- Palindromo.clases/palindromo.py
+++ Palindromo.clases/palindromo.py
@@ -1,5 +1,11 @@
-class Palíndromo:
-    @staticmethod
-    def esPalindromo(cadena):
-        cadena = cadena.lower().replace(" ", "").replace(",", "").replace(".", "")
-        return cadena == cadena[::-1]
+class Palindromo:
+    @classmethod
+    def esPalindromo(cls, cadena):
+        cadena = ''.join(e for e in cadena if e.isalnum())
+        return cadena.lower() == cadena[::-1].lower()
+print(Palindromo.esPalindromo('radar'))    
+print(Palindromo.esPalindromo('sonar'))   
+print(Palindromo.esPalindromo('Arde ya la yedra'))   
+print(Palindromo.esPalindromo('Ardeyalayedra'))   
+print(Palindromo.esPalindromo('!@#$% %$#@!'))  
+print(Palindromo.esPalindromo('L O L'))   
